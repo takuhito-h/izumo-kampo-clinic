677c5557fd69e5e46fb9711986dba551b424c97f
hihara takuhito <bluesbug@gmail.com>
2026-06-19 12:44:27 +0900

    [modi]indexに統一

--- src/components/navItems.js
+++ src/components/navItems.js
@@ -1,5 +1,5 @@
 export const navItems = [
-  { href: "/top.html", label: "ホーム" },
+  { href: "/", label: "ホーム" },
   { href: "/clinic.html", label: "クリニックのご案内" },
   { href: "/doctor.html", label: "医師のご紹介" },
   { href: "/kampo.html", label: "漢方治療について" },
